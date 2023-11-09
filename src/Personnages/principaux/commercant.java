871e0c9df466cfad3d6197f22b1fd2e59a25e0d2
TheoRe <theo.renauld@toulouse.miage.fr>
2023-11-09 17:43:33 +0100

    hh

--- src/Personnages/principaux/commercant.java
+++ src/Personnages/principaux/commercant.java
@@ -1,5 +1,19 @@
 package Personnages.principaux;
-
-public class commercant {
+import Personnages.Humain;
+public class commercant extends Humain {
+	
+	public commercant(String nom, int argent) {
+		super(nom,argent,"thé");
+	}
+	
+	public int seFaireExtorquer() {
+		this.perdreArgent(getArgent());
+		parler(" le monde est méchant\n");
+		return this.getArgent();
+	}
+	public void recevoir(int argent) {
+		parler("merci pour ton seub");
+		this.gagnerArgent(argent);
+	}
 
 }
